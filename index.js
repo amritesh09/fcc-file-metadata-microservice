fa5e4ace06d35fb15f6673c41e96352dae0317d9
Amritesh Srivastava <asriva3@L-IDC24KG3QC-M.local>
2017-01-28 18:21:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var path = require('path');
+var mongo = require('mongodb');
+var fs = require('fs');
+var fileUpload = require('express-fileupload');
+
+
+var app = express();
+app.use(fileUpload());
+
+mongo.MongoClient.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/url-shortener', function(err, db){
+	if(err){
+		throw new Erro('Failed to connect');
+	} else {
+		console.log('Successfully connected');
+	}
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.post('/upload', function(req, res){
+	console.log(req.files);
+	var file = req.files.displayImage;
+	file.mv(path.join(__dirname, 'uploads/file1.c'), function(err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+    else {
+    	console.log('uploaded successfully '+file.name);
+    	
+    	var stats = fs.statSync(path.join(__dirname, 'uploads/file1.c'));
+ 	 var fileSizeInBytes = stats["size"];
+ 	 console.log(stats);
+	res.send({
+		size : fileSizeInBytes
+	});
+      // res.send('File uploaded!');
+    }
+  });
+
+});
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+	console.log('Server listening on port : '+port);
+})
+
+
+});
+
+
+
+
+
+
+
